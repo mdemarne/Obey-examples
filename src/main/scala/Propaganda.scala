85efcc7f0c5ea07282426acce1052f7f59ed56ed
Mathieu Demarne <mathieu.demarne@gmail.com>
2015-04-26 14:32:30 +0200

    Covering new rule cases

--- src/main/scala/Propaganda.scala
+++ src/main/scala/Propaganda.scala
@@ -1,17 +1,18 @@
 package propaganda
-
 object Propaganda {
-
-  def main(args: Array[String]) {
+  def main(args: Array[String]): Unit = {
     println("Starting the Propaganda!")
-
-    var c = 3
-    val v1 = List(1,2,3).toSet
-    val v2 = Set('a','b','c').toList
+    val `c` = 3
+    val v1 = Set(1, 2, 3)
+    val v2 = Set('a', 'b', 'c').toList
     val h = new Hanoi(new Tower(0), new Tower(1), new Tower(2))
     h.init
-    println("Initial "+h)
+    println("Initial " + h)
     h.solve
-    println("Result "+h)
+    var x = Set(1,2,3,4)
+    println("Result " + h)
+    val y = Set(1,2,3,3)
+    val xx = x.toList
+    val yy = y.toList
   }
 }
